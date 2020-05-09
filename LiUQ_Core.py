8f47195780c9b37b536c6748b13c997dfcd8b5eb
PacoCosta <51125855+PacoCosta@users.noreply.github.com>
2020-05-09 22:27:28 +0200

    Update LiUQ_Core.py
    generating a dataframe

--- LiUQ_Core.py
+++ LiUQ_Core.py
@@ -20,7 +20,8 @@ import UQ_Hardware  # script with all calculations of hardware unc are done
 import UQ_Data_processing # script with all calculations of data processing methods unc are done
 import numpy as np
 import pdb
-import pickle	
+import pickle
+import itertools	
 
 #%% Read data from the GUI script:#######################
 #with open('I_D.pickle', 'rb') as c_data:
@@ -56,23 +57,30 @@ class Hardware_U():  # creating a function to call each different module. HAve t
                     UQ_Amp=UQ_Hardware.UQ_Amplifier(Atmospheric_inputs,Amplifier_uncertainty_inputs)
                     return UQ_Amp
                 def Amp_losses(self): # Calculation of losses in amplifier
-                    self.amp_losses=0
+                    global amp_losses
+                    self.amp_losses=[5,6]
                     return self.amp_losses
                 def Amp_others(self):
-                    self.amp_others=99
+                    global amp_others
+                    self.amp_others=[7,8]
                     return self.amp_others
             Obj=amplifier()#Create instance of object amplifier
-            Res['amplifier']=({'noise':Obj.Amp_noise(Atmospheric_inputs,Amplifier_uncertainty_inputs),'losses':Obj.Amp_losses(),'DELTA':Obj.Amp_others()})# Creating a nested dictionary
+            Res['amplifier']=({'amplifier_noise':Obj.Amp_noise(Atmospheric_inputs,Amplifier_uncertainty_inputs),'amplifier_losses':Obj.Amp_losses(),'amplifier_DELTA':Obj.Amp_others()})# Creating a nested dictionary
     if 'photodetector' in modules:
             class photodetector():
                 def Photo_noise(self,Atmospheric_inputs,Photodetector_uncertainty_inputs):
                     UQ_Photo=UQ_Hardware.UQ_Photodetector(Atmospheric_inputs,Photodetector_uncertainty_inputs)#function calculating amplifier uncertainties ((UQ_Photodetector.py))
                     return UQ_Photo   
                 def Photo_losses(self):
-                    self.photo_losses=13
+                    global photo_losses
+                    self.photo_losses=[9]
                     return self.photo_losses
+                def Photo_PACOEffects(self):
+                    global photo_PacoEffects
+                    self.photo_PacoEffects=[10]
+                    return self.photo_PacoEffects                
             Obj=photodetector()
-            Res['photodetector']=({'noise':Obj.Photo_noise(Atmospheric_inputs,Photodetector_uncertainty_inputs),'losses':Obj.Photo_losses()})
+            Res['photodetector']=({'photodetector_noise':Obj.Photo_noise(Atmospheric_inputs,Photodetector_uncertainty_inputs),'photodetector_losses':Obj.Photo_losses(),'photodetector_PAcoEffects':Obj.Photo_PACOEffects()})
                                          
     if 'telescope' in modules:
             class telescope():
@@ -80,13 +88,15 @@ class Hardware_U():  # creating a function to call each different module. HAve t
                     UQ_Tele=UQ_Hardware.UQ_Telescope(Atmospheric_inputs,Telescope_uncertainty_inputs)#function calculating amplifier uncertainties ((UQ_Telescope.py))
                     return UQ_Tele
                 def Tele_losses(self):
-                    self.tele_losses=1
+                    global tele_losses
+                    self.tele_losses=[1]
                     return self.tele_losses
                 def Tele_others(self):
-                    self.tele_others=33
+                    global tele_others
+                    self.tele_others=[33]
                     return self.tele_others
             Obj=telescope()
-            Res['telescope']=({'noise':Obj.Tele_noise(Atmospheric_inputs,Telescope_uncertainty_inputs),'losses':Obj.Tele_losses(),'DELTA':Obj.Tele_others()})
+            Res['telescope']=({'telescope_noise':Obj.Tele_noise(Atmospheric_inputs,Telescope_uncertainty_inputs),'telescope_losses':Obj.Tele_losses(),'telescope_DELTA':Obj.Tele_others()})
 
 #Create H_UQ dictionary of values: 
 
@@ -108,37 +118,120 @@ for method in DP:
     DP_UQ.append(Data_Processing_U(method=method))
 
 #%% Create a complete data frame (Hardware+data processing uncertainties): 
-Values_modules=list(H_UQ.values())
-Keys_modules=list(H_UQ.keys())
-Values_errors=[]
-Keys_errors=[]
+
 #Generate list of keys and values to loop over
-for i in range(len(Values_modules)):
-    Values_errors.append(list(Values_modules[i].values()))
-    Keys_errors.append(list(Values_modules[i].keys()))
-df_UQ= pd.DataFrame()
-keyCounter=0    
-#Loop over keys and values to generate the data frame:
-for key in Keys_modules:
-    subindices=[]
-    #Generate indexes of the data frame:     
-    for index in Keys_errors[keyCounter]:
-        subindices.append(key + ' ' + index)
-    #Generate the Data frame appending data frames from each module:
-    df_UQ=df_UQ.append(pd.DataFrame(Values_errors[keyCounter],columns=['Hardware (dB)'],index=subindices))
-    keyCounter+=1   
+Values_errors = [list(itertools.chain((H_UQ[i].values()))) for i in H_UQ.keys()]
+Keys_errors   = [list(itertools.chain((H_UQ[i].keys()))) for i in H_UQ.keys()]
+subindices=[]
+subcolumns=[]
+#Generate indexes of the data frame:
+subindices=list((itertools.chain(*Keys_errors)))
+# Generating columns of the data frame:
+subcolumns=[('T='+str(temp)+'; H='+str(hum)+'; Rain:'+str(rain)+'; Fog:'+str(fog) +'; error1='+str(error1)+'; error2='+ str(error2)+'; error3='+ str(error3)) for temp in Atmospheric_inputs['temperature'] for hum in Atmospheric_inputs['humidity'] for rain in Atmospheric_inputs['rain'] for fog in Atmospheric_inputs['fog'] for error1 in H_UQ['amplifier']['amplifier_losses'] for error2 in H_UQ['amplifier']['amplifier_DELTA'] for error3 in H_UQ['photodetector']['photodetector_losses'] ]
 
-in_dB=0
-#Sum af decibels:
-for valrows in range(0,df_UQ.shape[0]):
-    in_dB+=(10**(df_UQ.iloc[valrows,0]/10))
-Sum_decibels=10*np.log10(in_dB)
+Values_errors_removed=[list(itertools.product(*Values_errors[i][1:])) for i in range (len (Values_errors))] # values of the rest of errors (not related with atmospheric conditions) 
+fl_Values_errors_removed=list(itertools.product(*Values_errors_removed))
+fl2_Values_errors_removed=[list(itertools.chain(*fl_Values_errors_removed[Flat_ind])) for Flat_ind in range(len(fl_Values_errors_removed))]
+Values_errors_noise=[Values_errors[i][0] for i in range (len (Values_errors))]
+funcScenarios=lambda x:[x[i1][i2] for i1 in range(len(x))] 
+atmospheric_scenarios=[]
+for i2 in range(len(Values_errors_noise[0])):
+    atmospheric_scenarios.append(funcScenarios(Values_errors_noise))
+FinalScenarios0=list((itertools.product(atmospheric_scenarios,fl2_Values_errors_removed))) # Different scenarios for the different inputs (atmospheric-)
+FinalScenarios=[list(itertools.chain(*FinalScenarios0[ind_Scenario])) for ind_Scenario in range(len(FinalScenarios0)) ]
 
-#Transforming into watts
-df_UQ.loc['Total UQ','Hardware (dB)']= Sum_decibels# for now sum the uncertainties. Here have to apply Uncertainty expansion.
 
-#transform in watts. We supose that raw data is in dB:
-df_UQ['Hardware (w)']=(10**(df_UQ['Hardware (dB)']/10))
+
+
+#finalScenariostotal=list(itertools.chain(finalScenarios))
+
+df_UQ=pd.DataFrame(np.transpose(FinalScenarios),index=subindices,columns=subcolumns)    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Split_Val_err_mod=[]
+#for ind_val_err in range(len(Values_errors)):
+#    Val_err_mod=Values_errors[ind_val_err]
+#    Split_Val_err_mod.append(list(itertools.product(*Val_err_mod))) # Split the module errors of the atmospheric scenarios
+#    
+#df_UQ=pd.DataFrame(index=subindices,columns=subcolumns)    
+#
+#datacount=0
+#count2=0
+#
+#t=[len(H_UQ[i]) for i in H_UQ.keys()] # numer of key elements for the different modules (info about how many noise elements do we have per lidar module)
+#
+#
+#for ind3 in range(t[count2]):
+#    for ind2 in range(len(Split_Val_err_mod[ind3][0])):
+#        for ind1 in range(len(Split_Val_err_mod[ind3])):
+#            df_UQ.iloc[datacount,ind1]=(Split_Val_err_mod[ind3][ind1][ind2]) #allocate each value
+#        datacount+=1
+#    count2+=1
+
+
+
+
+
+
+
+#Generate the Data frame appending data frames from each module:
+#countt=0
+#for ind in range(len(subindices)):
+#    for ind2 in range(len(subcols)):
+#        df_UQ.iloc[ind, ind2]=Values_errors[0][0][0]
+#        countt+=1
+#
+#
+#
+#in_dB=0
+##Sum af decibels:
+#for valrows in range(0,df_UQ.shape[0]):
+#    in_dB+=(10**(df_UQ.iloc[valrows,0]/10))
+#Sum_decibels=10*np.log10(in_dB)
+#
+##Transforming into watts
+#df_UQ.loc['Total UQ','Hardware (dB)']= Sum_decibels# for now sum the uncertainties. Here have to apply Uncertainty expansion.
+#
+##transform in watts. We supose that raw data is in dB:
+#df_UQ['Hardware (w)']=(10**(df_UQ['Hardware (dB)']/10))
 
 
 
