676d8a2a9bac539e3e67ecfaa415a638b6424907
Costa <fcosta@IFB.Uni-Stuttgart.de>
2020-07-24 17:06:36 +0200

    Adding new version of Qlunc
    The new version is more objected oriented and flexible.
    We can ask for components, modules or lidar uncertainties. The code doesnt have to go over the whole code. Can perform specific uncertainties calculations from specific lidar device.

---
+++ Qlunc_CLASSES/Qlunc_Help_standAlone.py
@@ -0,0 +1,112 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 18 00:03:43 2020
+
+@author: fcosta
+"""
+
+from Qlunc_ImportModules import *
+#import Qlunc_Help_standAlone as SA
+#from Main.Qlunc_inputs import inputs
+
+#%%# used to flatt in som points along the code:
+flatten = lambda *n: (e for a in n for e in (flatten(*a) if isinstance(a, (list,tuple)) else (a,))) 
+
+#%%# Get_Components is a function that extracts, for each module inside the class Hardware_U, in order to include 
+#    in calculations, the components and the uncertainties (noise_type)
+def Get_Components(module,CLASS,METHODS):
+    
+    for components in METHODS.keys(): #for components        
+        CLASS[components]={}            
+        for noise_type in inputs.modules[module][components]:
+            CLASS[components][noise_type]=METHODS[components][noise_type]
+    return CLASS
+
+
+#%% Sum of dB:
+def Sum_dB(W_data,*args,**kwargs):
+   #Sum af decibels:
+    pdb.set_trace()
+    Sumat= []
+    Sum_decibels=[]
+    for ii in W_data:
+        gg=((10**(ii/10)))    
+        Sumat.append (gg)
+    Sum_in_dB = sum(Sumat)
+    Sum_decibels.append(10*np.log10(Sum_in_dB) )
+#    pdb.set_trace()
+    return list (flatten(Sum_decibels))
+
+#%% Combine uncertainties:
+def unc_comb(data): # data is provided as list of elements want to add on
+    sqr=[]    
+    sqr_db=[]
+    Array=(np.array(data)) 
+#    pdb.set_trace()
+    for i in range(np.shape(Array)[1]):
+        sq=[]
+        for ii in range (len(Array)):
+            sq.append(Array[ii,i]**2) # make the square of each value
+        sqr.append(np.sqrt(sum(sq))) #Sqrt of the values in the column
+    for i_w in sqr:
+        sqr_db.append(10*np.log10(i_w))
+    return sqr_db
+# %% Getting data frame:
+    
+def Get_DataFrame(H_UQ,Temperature):  
+#    pdb.set_trace()
+    indexesDF=list(H_UQ.keys())
+    
+#    for fromModkeys in list(H_UQ.keys()):    
+#        indexesDF
+##        indexesDF.update((reduce(getitem,[fromModkeys],H_UQ)))
+#    indexesDF=list(indexesDF.keys())
+    columnsDF=['T= {} °K'.format(Temperature[i]) for i in range(len(Temperature))] # Getting data frame columns          
+#    pdb.set_trace()
+#    Full_df=([H_UQ[components].get(noise_type,{}) for components  in H_UQ.keys() for noise_type in H_UQ [components].keys() ])
+    
+    Full_df=pd.DataFrame(H_UQ, columns=indexesDF,index=columnsDF)
+#    Full_df=Full_df.T
+#    #Sum af decibels:
+#    in_dB=0
+#    Sum_decibels= []
+#    for valcols in range(0,Full_df.shape[1]):
+#        Sum_in_dB     = sum([(10**(Full_df.iloc[valrows,valcols]/10)) for valrows in range(0,Full_df.shape[0])])
+#    #    Sum_in_dB = sum(in_dB)
+#        Sum_decibels.append(10*np.log10(Sum_in_dB) )
+#    
+#    
+#    Full_df.loc['Total UQ']= Sum_decibels# for now sum the uncertainties. Here have to apply Uncertainty expansion.
+#    
+#
+#   #transform in watts. We supose that raw data is in dB:
+#    df_UQ['Hardware (w)']=(10**(df_UQ['Hardware (dB)']/10))
+
+    return Full_df  
+
+
+#%% Spherical into cartesian  coordinate transformation
+def sph2cart(Lidar,rho, theta,phi): 
+#    pdb.set_trace()
+    x=[]
+    y=[]
+    z=[]
+    for i in range(len(rho)):
+        x=rho[i]*np.cos(np.deg2rad(Lidar.optics.scanner.phi))*np.sin(np.deg2rad(Lidar.optics.scanner.theta))
+        y=rho[i]*np.sin(np.deg2rad(Lidar.optics.scanner.phi))*np.sin(np.deg2rad(Lidar.optics.scanner.theta)) 
+        z=rho[i]*np.cos(np.deg2rad(Lidar.optics.scanner.theta)) 
+    return(x,y,z)
+
+    # POLARS:
+    #def pol2cart(rho, phi):
+    #    xcart= rho * np.cos(phi)
+    #    ycart = rho * np.sin(phi)
+    #    return(xcart, ycart)
+        
+    # SPHERICAL:
+    #def sph2cart(rho, theta,phi): # Spherical into cartesian  coordinate transformation
+    #    xcart = rho * np.cos(phi)*np.sin(theta)
+    #    ycart = rho * np.sin(phi)*np.sin(theta)
+    #    zcart = rho*np.cos(theta)
+    #    return(xcart, ycart,zcart) 
+
---
+++ Qlunc_CLASSES/UQ_Photonics_Classes.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 20 14:15:24 2020
+
+@author: fcosta
+"""
+from Qlunc_ImportModules import *
+import Qlunc_Help_standAlone as SA
+import pandas as pd
+import scipy.interpolate as itp
+
+#%% PHOTODETECTOR
+def UQ_Photodetector(Lidar, Atmospheric_Scenario,cts):
+    UQ_Photodetector.Thermal_noise=[]
+    UQ_Photodetector.SNR_thermal_noise=[]
+    UQ_Photodetector.Shot_noise=[]
+    UQ_Photodetector.SNR_Shot_noise=[]
+    UQ_Photodetector.Dark_current_noise=[]
+    UQ_Photodetector.SNR_DarkCurrent=[]
+    UQ_Photodetector.UQ_Photo=[]
+    UQ_Photodetector.SNR_TIA=[]
+    UQ_Photodetector.TIA_noise=[]
+    R = Lidar.photonics.photodetector.Efficiency*cts.e*Lidar.lidar_inputs.Wavelength/(cts.h*cts.c)  #[W/A]  Responsivity
+    UQ_Photodetector.Responsivity = (R) # this notation allows me to get Responsivity from outside of the function 
+
+    for i in range(len(Atmospheric_Scenario.temperature)):
+        # Photodetector Thermal noise
+        UQ_Photodetector.Thermal_noise.append(4*cts.k*Atmospheric_Scenario.temperature[i]/Lidar.photonics.photodetector.RL*Lidar.photonics.photodetector.BandWidth) #[dBm]
+        UQ_Photodetector.SNR_thermal_noise.append(((R**2)/(4*cts.k*Atmospheric_Scenario.temperature[i]*Lidar.photonics.photodetector.BandWidth/Lidar.photonics.photodetector.RL))*(Lidar.photonics.photodetector.SignalPower/1000)**2)
+    
+    #Photodetector shot noise:
+    UQ_Photodetector.Shot_noise     = [(2*cts.e*R*Lidar.photonics.photodetector.BandWidth*Lidar.photonics.photodetector.SignalPower)]*len(Atmospheric_Scenario.temperature) 
+    UQ_Photodetector.SNR_Shot_noise = [(((R**2)/(2*cts.e*R*Lidar.photonics.photodetector.BandWidth))*(Lidar.photonics.photodetector.SignalPower)/1000)]*len(Atmospheric_Scenario.temperature) 
+    #Photodetector dark current noise
+    UQ_Photodetector.Dark_current_noise = [(2*cts.e*Lidar.photonics.photodetector.DarkCurrent*Lidar.photonics.photodetector.BandWidth*Lidar.photonics.photodetector.SignalPower)]*len(Atmospheric_Scenario.temperature) 
+    UQ_Photodetector.SNR_DarkCurrent    = [(((R**2)/(2*cts.e*Lidar.photonics.photodetector.DarkCurrent*Lidar.photonics.photodetector.BandWidth))*((Lidar.photonics.photodetector.SignalPower/1000)**2))]*len(Atmospheric_Scenario.temperature) 
+#    pdb.set_trace()
+    
+    if any(TIA_val == None for TIA_val in [Lidar.photonics.photodetector.Gain_TIA,Lidar.photonics.photodetector.V_Noise_TIA]): # If any value of TIA is None dont include TIA noise in estimations :
+        UQ_Photodetector.UQ_Photo = [(SA.unc_comb([UQ_Photodetector.Thermal_noise,UQ_Photodetector.Shot_noise,UQ_Photodetector.Dark_current_noise]))]
+    else:
+        # Photodetector TIA noise
+        UQ_Photodetector.TIA_noise = [(Lidar.photonics.photodetector.V_Noise_TIA**2/Lidar.photonics.photodetector.Gain_TIA**2)]*len(Atmospheric_Scenario.temperature) 
+        UQ_Photodetector.SNR_TIA   = [(((R**2)/(Lidar.photonics.photodetector.V_Noise_TIA**2/Lidar.photonics.photodetector.Gain_TIA**2))*(Lidar.photonics.photodetector.SignalPower/1000)**2)]*len(Atmospheric_Scenario.temperature) 
+#        pdb.set_trace()
+        UQ_Photodetector.UQ_Photo.append(SA.unc_comb([UQ_Photodetector.Thermal_noise,UQ_Photodetector.Shot_noise,UQ_Photodetector.Dark_current_noise,UQ_Photodetector.TIA_noise]))
+    UQ_Photodetector.UQ_Photo=list(SA.flatten(UQ_Photodetector.UQ_Photo))        
+#    pdb.set_trace()
+    return UQ_Photodetector.UQ_Photo
+
+
+
+#%% OPTICAL AMPLIFIER
+def UQ_Optical_amplifier(Lidar,Atmospheric_Scenario,cts): # Calculating ASE - Amplified Spontaneous Emission definition ((**Optics and Photonics) Bishnu P. Pal - Guided Wave Optical Components and Devices_ Basics, Technology, and Applications -Academic Press (2005))
+    try:
+    # obtain SNR from figure noise or pass directly numerical value:
+        if isinstance (Lidar.photonics.optical_amp.NoiseFig, numbers.Number): #If user introduces a number or a table of values
+            FigureNoise=[Lidar.photonics.optical_amp.NoiseFig]*len(Atmospheric_Scenario.temperature)
+        else:
+            NoiseFigure_DATA = pd.read_csv(Lidar.photonics.optical_amp.NoiseFig,delimiter=';',decimal=',') #read from a .csv file variation of dB with wavelength (for now just with wavelength)    
+            figure_noise_INT  = itp.interp1d(NoiseFigure_DATA.iloc[:,0],NoiseFigure_DATA.iloc[:,1],kind='cubic',fill_value="extrapolate")# First column wavelength,second column SNR in dB
+            NoiseFigure_VALUE = figure_noise_INT(Lidar.lidar_inputs.Wavelength) # in dB
+            FigureNoise=(NoiseFigure_VALUE.tolist())
+        
+        UQ_Optical_amplifier = [10*np.log10((10**(FigureNoise/10))*cts.h*(cts.c/Lidar.lidar_inputs.Wavelength)*10**(Lidar.photonics.optical_amp.Gain/10))]*len(Atmospheric_Scenario.temperature) # ASE
+    except:
+        print('No Optical Amplifier implemented')
+    return UQ_Optical_amplifier
+
+#%% Sum of components in photonics module
+def sum_unc_photonics(Lidar,Atmospheric_Scenario,cts): 
+    try: # ecah try/except evaluates wether the component is included
+#        if Photodetector_Uncertainty not in locals():
+        Photodetector_Uncertainty=Lidar.photonics.photodetector.Uncertainty(Lidar,Atmospheric_Scenario,cts)
+    except:
+        Photodetector_Uncertainty=None
+        print('No photodetector in calculations!')
+    try:
+        Optical_Amplifier_Uncertainty=Lidar.photonics.optical_amp.Uncertainty(Lidar,Atmospheric_Scenario,cts)
+    except:
+        Optical_Amplifier_Uncertainty=None
+        print('No OA in calculations!')
+#    pdb.set_trace()
+    
+    
+    List_Unc_photonics1=[]
+    List_Unc_photonics0=[Photodetector_Uncertainty,Optical_Amplifier_Uncertainty]
+    for x in List_Unc_photonics0:
+        
+        if isinstance(x,list):
+           
+            List_Unc_photonics0=([10**(i/10) for i in x]) # Make the list without None values and convert in watts(necessary for SA.unc_comb)
+            List_Unc_photonics1.append([List_Unc_photonics0]) # Make a list suitable for unc.comb function
+#    pdb.set_trace()
+
+    Uncertainty_Photonics_Module=SA.unc_comb(List_Unc_photonics1)
+    return list(SA.flatten(Uncertainty_Photonics_Module))
