3475919ed43ef88f70748e6e9131d2a0d20bc219
Costa <fcosta@IFB.Uni-Stuttgart.de>
2020-08-04 14:08:01 +0200

    Creating Lidar uncertainty script
    Lidar uncertainty script to gather all uncertainties comming from different sources

---
+++ Qlunc_CLASSES/UQ_Lidar_Classes.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug  4 13:55:41 2020
+
+@author: fcosta
+"""
+
+# Calculates the lidar global uncertainty:
+
+def sum_unc_lidar(Lidar,Atmospheric_Scenario,cts): 
+    try: # ecah try/except evaluates wether the component is included in the module, therefore in the calculations
+#        if Photodetector_Uncertainty not in locals():
+        Photonics_Uncertainty=Lidar.photonics.Uncertainty(Lidar,Atmospheric_Scenario,cts)
+    except:
+        Photonics_Uncertainty=None
+        print('No photonics module in calculations!')
+    try:
+        Optics_Uncertainty=Lidar.optics.Uncertainty(Lidar,Atmospheric_Scenario,cts)
+    except:
+        Optics_Uncertainty=None
+        print('No optics module in calculations!')
+
+    try:
+        Power_Uncertainty=Lidar.power.Uncertainty(Lidar,Atmospheric_Scenario,cts)
+    except:
+        Power_Uncertainty=None
+        print('No power module in calculations!')
