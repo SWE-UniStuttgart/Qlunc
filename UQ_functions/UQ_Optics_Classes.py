20f303331f1594877d86c5248e042b875de796ed
Francisco Costa <costa@ifb.uni-stuttgart.de>
2021-01-20 09:20:47 +0100

    Update UQ_Optics_Classes.py
    Introduce VAD options in evrsion V-0.9

--- UQ_functions/UQ_Optics_Classes.py
+++ UQ_functions/UQ_Optics_Classes.py
@@ -13,7 +13,7 @@ import Qlunc_Help_standAlone as SA
 #import scipy.interpolate as itp
 #import pdb
 
-def UQ_Telescope(Lidar, Atmospheric_Scenario,cts):
+def UQ_Telescope(Lidar, Atmospheric_Scenario,cts): #This is not correct yet. Just implemented as an example
 #    toreturn={}
     UQ_telescope=[(temp*0.5+hum*0.1+curvature_lens*0.1+aberration+o_c_tele) \
                   for temp           in inputs.atm_inp.Atmospheric_inputs['temperature']\
@@ -28,7 +28,7 @@ def UQ_Telescope(Lidar, Atmospheric_Scenario,cts):
     Final_Output_UQ_Telescope={'Uncertainty_Telescope':UQ_telescope}
     return Final_Output_UQ_Telescope
     
-def UQ_Scanner(Lidar, Atmospheric_Scenario,cts):
+def UQ_Scanner(Lidar, Atmospheric_Scenario,cts,Qlunc_yaml_inputs):
     Coord=[]
     Mean_Stdv_DISTANCE=[]  
     SimMean_DISTANCE=[]
@@ -39,16 +39,39 @@ def UQ_Scanner(Lidar, Atmospheric_Scenario,cts):
     NoisyZ=[]
     coun=0
     sample_rate_count=0
-    for fd_or,cone_angle_or,azimuth_or in zip(Lidar.optics.scanner.focus_dist,Lidar.optics.scanner.cone_angle,Lidar.optics.scanner.azimuth):# Take coordinates from inputs
+    
+    # Differentiate between 'VAD' or 'Forward-looking nacelle-mounted' lidar depending on users choice:
+    if Qlunc_yaml_inputs['Components']['Scanner']['Type']=='VAD':
+        param1=Lidar.optics.scanner.focus_dist
+        param2=Lidar.optics.scanner.cone_angle
+        param3=Lidar.optics.scanner.azimuth
+        stdv_param1=Lidar.optics.scanner.stdv_focus_dist
+        stdv_param2=Lidar.optics.scanner.stdv_cone_angle
+        stdv_param3=Lidar.optics.scanner.stdv_azimuth
+        
+    elif Qlunc_yaml_inputs['Components']['Scanner']['Type']=='FLN':
+        param1=Lidar.optics.scanner.x
+        param2=Lidar.optics.scanner.y
+        param3=Lidar.optics.scanner.z
+        stdv_param1=Lidar.optics.scanner.stdv_x
+        stdv_param2=Lidar.optics.scanner.stdv_y
+        stdv_param3=Lidar.optics.scanner.stdv_z
+        
+    for param1_or,param2_or,param3_or in zip(param1,param2,param3):# Take coordinates from inputs
         Mean_DISTANCE=[]
         DISTANCE=[]        
         stdv_DISTANCE=[]  
         sample_rate_count+=Lidar.optics.scanner.sample_rate
         
-        #Calculating the theoretical point coordinate transformation (conversion from spherical to cartesians):
-        x0=(fd_or)*np.cos(np.deg2rad(azimuth_or))*np.sin(np.deg2rad(cone_angle_or))+Lidar.optics.scanner.origin[0]
-        y0=(fd_or)*np.sin(np.deg2rad(azimuth_or))*np.sin(np.deg2rad(cone_angle_or))+ Lidar.optics.scanner.origin[1]
-        z0=(fd_or)*np.cos(np.deg2rad(cone_angle_or))+Lidar.optics.scanner.origin[2]+sample_rate_count
+        #Calculating the theoretical point coordinate transformation (conversion from spherical to cartesians if 'VAD' is chosen):
+        if Qlunc_yaml_inputs['Components']['Scanner']['Type']=='VAD':
+            x0 = (param1_or)*np.cos(np.deg2rad(param3_or))*np.sin(np.deg2rad(param2_or)) + Lidar.optics.scanner.origin[0]
+            y0 = (param1_or)*np.sin(np.deg2rad(param3_or))*np.sin(np.deg2rad(param2_or)) + Lidar.optics.scanner.origin[1]
+            z0 = (param1_or)*np.cos(np.deg2rad(param2_or)) + Lidar.optics.scanner.origin[2] + sample_rate_count
+        elif Qlunc_yaml_inputs['Components']['Scanner']['Type']=='FLN':
+            x0 = param1_or + Lidar.optics.scanner.origin[0] + sample_rate_count
+            y0 = param2_or + Lidar.optics.scanner.origin[1] 
+            z0 = param3_or + Lidar.optics.scanner.origin[2] 
         #Storing coordinates
         X0.append(x0)
         Y0.append(y0)
@@ -58,40 +81,44 @@ def UQ_Scanner(Lidar, Atmospheric_Scenario,cts):
             
         # Create white noise with stdv selected by user for each pointing input
             n=10000 # Number of cases to combine
-            del_focus_dist = np.array(np.random.normal(0 ,Lidar.optics.scanner.stdv_focus_dist,n)) # why a normal distribution??Does it have sense, can be completely random?
-            del_cone_angle = np.array(np.random.normal(0,Lidar.optics.scanner.stdv_cone_angle,n))
-            del_azimuth    = np.array(np.random.normal(0,Lidar.optics.scanner.stdv_azimuth,n))
+            del_param1 = np.array(np.random.normal(0,stdv_param1,n)) # why a normal distribution??Does it have sense, can be completely random?
+            del_param2 = np.array(np.random.normal(0,stdv_param2,n))
+            del_param3 = np.array(np.random.normal(0,stdv_param3,n))
             
     #        Adding noise to the theoretical position:
-            noisy_fd         = fd_or    + del_focus_dist
-            noisy_cone_angle = cone_angle_or + del_cone_angle 
-            noisy_azimuth    = azimuth_or   + del_azimuth 
+            noisy_param1 = param1_or + del_param1
+            noisy_param2 = param2_or + del_param2 
+            noisy_param3 = param3_or + del_param3 
             
-#            Cartesian coordinates of the noisy points:            
-            x=noisy_fd*np.cos(np.deg2rad(noisy_azimuth))*np.sin(np.deg2rad(noisy_cone_angle))
-            y=noisy_fd*np.sin(np.deg2rad(noisy_azimuth))*np.sin(np.deg2rad(noisy_cone_angle)) 
-            z=noisy_fd*np.cos(np.deg2rad(noisy_cone_angle)) + sample_rate_count
-            
+#            Cartesian coordinates of the noisy points when VAD, else coordinate system remains cartesian:            
+            if Qlunc_yaml_inputs['Components']['Scanner']['Type']=='VAD':
+                x = noisy_param1*np.cos(np.deg2rad(noisy_param3))*np.sin(np.deg2rad(noisy_param2))
+                y = noisy_param1*np.sin(np.deg2rad(noisy_param3))*np.sin(np.deg2rad(noisy_param2)) 
+                z = noisy_param1*np.cos(np.deg2rad(noisy_param2)) + sample_rate_count
+            elif Qlunc_yaml_inputs['Components']['Scanner']['Type']=='FLN':
+                x = noisy_param1 + sample_rate_count
+                y = noisy_param2
+                z = noisy_param3                
             # Implement error in deployment of the tripod as a rotation over yaw, pitch and roll
 
             R=[[np.cos(np.deg2rad(Lidar.lidar_inputs.yaw_error_dep))*np.cos(np.deg2rad(Lidar.lidar_inputs.pitch_error_dep)),  np.cos(np.deg2rad(Lidar.lidar_inputs.yaw_error_dep))*np.sin(np.deg2rad(Lidar.lidar_inputs.pitch_error_dep))*np.sin(np.deg2rad(Lidar.lidar_inputs.roll_error_dep))-np.sin(np.deg2rad(Lidar.lidar_inputs.yaw_error_dep))*np.cos(np.deg2rad(Lidar.lidar_inputs.roll_error_dep)),  np.cos(np.deg2rad(Lidar.lidar_inputs.yaw_error_dep))*np.sin(np.deg2rad(Lidar.lidar_inputs.pitch_error_dep))*np.cos(np.deg2rad(Lidar.lidar_inputs.roll_error_dep))+np.sin(np.deg2rad(Lidar.lidar_inputs.yaw_error_dep))*np.sin(np.deg2rad(Lidar.lidar_inputs.roll_error_dep))],
                [np.sin(np.deg2rad(Lidar.lidar_inputs.yaw_error_dep))*np.cos(np.deg2rad(Lidar.lidar_inputs.pitch_error_dep)),  np.sin(np.deg2rad(Lidar.lidar_inputs.yaw_error_dep))*np.sin(np.deg2rad(Lidar.lidar_inputs.pitch_error_dep))*np.sin(np.deg2rad(Lidar.lidar_inputs.roll_error_dep))+np.cos(np.deg2rad(Lidar.lidar_inputs.yaw_error_dep))*np.cos(np.deg2rad(Lidar.lidar_inputs.roll_error_dep)),  np.sin(np.deg2rad(Lidar.lidar_inputs.yaw_error_dep))*np.sin(np.deg2rad(Lidar.lidar_inputs.pitch_error_dep))*np.cos(np.deg2rad(Lidar.lidar_inputs.roll_error_dep))-np.cos(np.deg2rad(Lidar.lidar_inputs.yaw_error_dep))*np.sin(np.deg2rad(Lidar.lidar_inputs.roll_error_dep))],
                [       -np.sin(np.deg2rad(Lidar.lidar_inputs.pitch_error_dep))                                             ,                      np.cos(np.deg2rad(Lidar.lidar_inputs.pitch_error_dep))*np.sin(np.deg2rad(Lidar.lidar_inputs.roll_error_dep))                                                                                                                                            ,                                                                np.cos(np.deg2rad(Lidar.lidar_inputs.pitch_error_dep))*np.cos(np.deg2rad(Lidar.lidar_inputs.roll_error_dep))]]
             
-            xfinal = np.matmul(R,[x,y,z])[0]+Lidar.optics.scanner.origin[0]
-            yfinal = np.matmul(R,[x,y,z])[1]+Lidar.optics.scanner.origin[1]
-            zfinal = np.matmul(R,[x,y,z])[2]+Lidar.optics.scanner.origin[2]
+            xfinal = np.matmul(R,[x,y,z])[0] + Lidar.optics.scanner.origin[0]
+            yfinal = np.matmul(R,[x,y,z])[1] + Lidar.optics.scanner.origin[1]
+            zfinal = np.matmul(R,[x,y,z])[2] + Lidar.optics.scanner.origin[2]
 
 #            pdb.set_trace()
             # Distance between theoretical measured points and noisy points:
             DISTANCE.append(np.sqrt((xfinal-x0)**2+(yfinal-y0)**2+(zfinal-z0)**2))
             Mean_DISTANCE.append(np.mean(DISTANCE[trial]))    
             stdv_DISTANCE.append(np.std(DISTANCE[trial]))
-            
+        sample_rate_count+=Lidar.optics.scanner.sample_rate    
         SimMean_DISTANCE.append(np.mean(DISTANCE))   # Mean error distance of each point in the pattern  
         Mean_Stdv_DISTANCE.append(np.mean(stdv_DISTANCE)) # Mean error distance stdv for each point in the pattern
         # Want to create a noise to add to the theoretical position to simulate the error in measurements
-        #Storing coordinates
+        # Storing coordinates:
         X.append(xfinal)
         Y.append(yfinal)
         Z.append(zfinal)
@@ -126,6 +153,7 @@ def UQ_OpticalCirculator(Lidar,Atmospheric_Scenario,cts):
     Optical_Circulator_Uncertainty = [Lidar.optics.optical_circulator.insertion_loss]
     Final_Output_UQ_Optical_Circulator={'Optical_Circulator_Uncertainty':Optical_Circulator_Uncertainty}
     return Final_Output_UQ_Optical_Circulator
+
 #%% Sum of uncertainty components in optics module: 
 def sum_unc_optics(Lidar,Atmospheric_Scenario,cts):
     List_Unc_optics = []
