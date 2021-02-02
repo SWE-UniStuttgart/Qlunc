984dbafd523e347ed26c6edd2887b6a56f4dcb77
Francisco Costa <costa@ifb.uni-stuttgart.de>
2021-02-02 16:07:44 +0100

    Update Qlunc_Help_standAlone.py
    data_db instead data2 (clearer terminology)

--- Utils/Qlunc_Help_standAlone.py
+++ Utils/Qlunc_Help_standAlone.py
@@ -18,26 +18,27 @@ flatten = lambda *n: (e for a in n for e in (flatten(*a) if isinstance(a, (list,
 
 
 #%% Combine uncertainties:
-def unc_comb(data): # data is provided as a list of elements want to add on. Data is expected to be in dB (within this functions dB are transformed into watts). The uncertainty combination is made following GUM
+# The uncertainty combination is made following GUM
+def unc_comb(data): # data is provided as a list of elements want to add on. Data is expected to be in dB (within this functions dB are transformed into watts).
     data_watts  = []
     res_dB      = []
     res_watts   = []
     zipped_data = []
     if not isinstance (data,np.ndarray):
         data=np.array(data)    
-    for data_row in range(np.shape(data)[0]):# trnasform into watts
+    for data_row in range(np.shape(data)[0]):# transform into watts
         
         try:    
-            data2=data[data_row,:]
+            data_db=data[data_row,:]
         except:
-            data2=data[data_row][0]
+            data_db=data[data_row][0]
              
-        data_watts.append(10**(data2/10))
+        data_watts.append(10**(data_db/10))
     for i in range(len(data_watts[0])): # combining all uncertainties making sum of squares and the sqrt of the sum
         zipped_data.append(list(zip(*data_watts))[i])
         res_watts.append(np.sqrt(sum(map (lambda x: x**2,zipped_data[i]))))
         res_dB=10*np.log10(res_watts)
-    del data2
+    del data_db
     return np.array(res_dB)
 
 #%% Spherical into cartesian  coordinate transformation
