8ce73dd8184e82cffd9a15f225201c40b46c9b7c
Francisco Costa <51125855+PacoCosta@users.noreply.github.com>
2021-03-03 12:56:29 +0100

    Update Qlunc_Instantiate.py

--- Main/Qlunc_Instantiate.py
+++ Main/Qlunc_Instantiate.py
@@ -24,6 +24,8 @@ could be done by instantiating their python classes:
 """
 
 #%% Running Qlunc_Classes.py:
+
+# Reading data from the yaml file:
 from Qlunc_Classes import *
 with open (r'./Qlunc_inputs.yml') as file: # WHere the yaml file is in order to get the input data
     Qlunc_yaml_inputs={}
