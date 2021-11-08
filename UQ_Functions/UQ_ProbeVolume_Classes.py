b724c1145ca226588a98098701dc6d0292444c2f
Francisco Costa <costa@ifb.uni-stuttgart.de>
2021-11-08 20:10:41 +0100

    Update UQ_ProbeVolume_Classes.py
    including calculation of probe volume for the case of pulsed lidar

--- UQ_Functions/UQ_ProbeVolume_Classes.py
+++ UQ_Functions/UQ_ProbeVolume_Classes.py
@@ -30,7 +30,7 @@ def UQ_Probe_volume (Lidar, Atmospheric_Scenario,cts,Qlunc_yaml_inputs):
         Unc_a0                    = Qlunc_yaml_inputs['Components']['Telescope']['stdv Fiber-lens offset']
         Unc_wavelength            = Qlunc_yaml_inputs['Components']['Laser']['stdv Wavelength']
         Unc_eff_radius_telescope  = Qlunc_yaml_inputs['Components']['Telescope']['stdv Effective radius telescope']
-        
+        # pdb.set_trace()
         # Focus distance
         focus_distance = 1/((1/r)-(1/(a+a0))) 
         
@@ -64,54 +64,39 @@ def UQ_Probe_volume (Lidar, Atmospheric_Scenario,cts,Qlunc_yaml_inputs):
         phi = (Qlunc_yaml_inputs['Probe Volume']['Extinction coeficient']/np.pi)*(1/((1**2)+(focus_distance)**2))
 
         # F = (lamb/np.pi)/(a1**2+lamb**2)  # Lorentzian Weighting function 
-    elif Qlunc_yaml_inputs['Components']['Lidar general inputs']['Type']=="Pulsed":
-        # for pulsed lidar we have a constant probe volume
-        print("pulsed lidar probe volume is a convolution between pulse shape and weighting function. Not inplemented yet")
-        
     
-    Final_Output_UQ_ProbeVolume = {'Rayleigh Length':zr,'Rayleigh Length uncertainty':Unc_zr,'Focus Distance':focus_distance,'Focus Distance uncertainty':Unc_focus_distance}
+    elif Qlunc_yaml_inputs['Components']['Lidar general inputs']['Type']=="Pulsed":
+        # Definition from LEOSPHERE pulsed principles - "pulsed lidar probe volume is a convolution between pulse shape and weighting function.
+        # zr = (cts.c*Lidar.optics.telescope.tau_meas)/(2*math.erf(np.sqrt(np.log(2))*(Lidar.optics.telescope.tau_meas)/(Lidar.optics.telescope.tau)))/2
+        
+        # Variables
+        tau_meas= Qlunc_yaml_inputs['Components']['Telescope']['Range gate']
+        tau= Qlunc_yaml_inputs['Components']['Telescope']['Pulse shape']
+        focus_distance = random.randrange(1,500,1)
+        
+        # Weighting function calculation
+        zr=[]
+        offset = 500
+        focus_distance = 0
+        z=np.linspace(focus_distance-offset,focus_distance+offset,1001)
+        for i in z:
+        
+            zr.append((1/(tau_meas*cts.c))*(math.erf((4*np.sqrt(np.log(2))*(i-focus_distance)/((cts.c*tau)))+(np.sqrt(np.log(2)))*tau_meas/tau)-math.erf((4*np.sqrt(np.log(2))*(i-focus_distance)/((cts.c*tau)))-(np.sqrt(np.log(2)))*tau_meas/tau)))
+        
+        # find the two crossing points
+        hmx = SA.half_max_x(z,zr)
+        
+        # print the answer
+        fwhm = hmx[1] - hmx[0]
+        print("FWHM:{:.3f}".format(fwhm))
+        
+    pdb.set_trace()
+    
+    Final_Output_UQ_ProbeVolume = {'Rayleigh Length':zr}#,'Rayleigh Length uncertainty':Unc_zr,'Focus Distance':focus_distance,'Focus Distance uncertainty':Unc_focus_distance}
 
     # Plotting:
     
     QPlot.plotting(Lidar,Qlunc_yaml_inputs,Final_Output_UQ_ProbeVolume,False,False,Qlunc_yaml_inputs['Flags']['Probe Volume parameters'],False)
     return Final_Output_UQ_ProbeVolume
-    #%% ################################ FWHM ##############################
-    
-    # Method to calculate FWHM
-    # def lin_interp(x, y, i, half):
-    #     return x[i] + (x[i+1] - x[i]) * ((half - y[i]) / (y[i+1] - y[i]))
-    
-    # def half_max_x(x, y):
-    #     half = max(y)/2.0
-    #     signs = np.sign(np.add(y, -half))
-    #     zero_crossings = (signs[0:-2] != signs[1:-1])
-    #     zero_crossings_i = np.where(zero_crossings)[0]
-    #     # plt.figure()
-    #     # plt.plot(dist,F,  linewidth=3)    
-    #     return [lin_interp(x, y, zero_crossings_i[0], half),
-    #             lin_interp(x, y, zero_crossings_i[1], half)]
-    # hmx = half_max_x(a1,F)
-    # fwhm = hmx[1] - hmx[0]
-    
-    # # ###################################################################
-    
-    # # #%% Probe volume
-    
-    
-    # #%% Uncertainty:
-    # Unc_R=0.1  # uncertainty in focus distance
-    # Unc_wave=1e-9 # Uncertainty in wavelength
-    # Unc_aperture=3e-6 # uncertainty in beam radius at focus length 
-    # Unc_PV=np.sqrt(((8*focus_distance*wave/aperture)*Unc_R)**2+((4*(focus_distance**2)/aperture)*Unc_wave)**2+((4*(focus_distance**2)*wave/(aperture**2))*Unc_aperture)**2)  # Uncertainty in probe volume
-    
-    # print('Lamb: {:.3f}'.format(lamb))
-    # # print("FWHM: {:.3f}".format(fwhm))
-    # print("ProbeVolume: {:.3f}".format(Probe_volume))
-    # print("Uncertainty PV: {:.3f}".format(Unc_PV))
-    # print("Error[%]: {:.3f}".format(100*Unc_PV/Probe_volume))
-    # # print("Cilinder Volume:{:.3f}".format(VolCil))
-    
-    
-
-    
+ 
     
