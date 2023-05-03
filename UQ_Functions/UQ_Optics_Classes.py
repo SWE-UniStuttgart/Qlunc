780b50133c8fe89e5f377cba069daaa9e42afa3d
Francisco Costa <costa@ifb.uni-stuttgart.de>
2023-05-03 21:16:38 +0200

    Update UQ_Optics_Classes.py

--- UQ_Functions/UQ_Optics_Classes.py
+++ UQ_Functions/UQ_Optics_Classes.py
@@ -72,7 +72,7 @@ def UQ_Scanner(Lidar, Atmospheric_Scenario,cts,Qlunc_yaml_inputs):
         rho0           = [Lidar.optics.scanner.focus_dist[meas_param]]  
         theta0         = [np.radians(Lidar.optics.scanner.cone_angle[meas_param])]  # %(360) converts negative angles to [0,360] positive angles
         psi0           = [np.radians(Lidar.optics.scanner.azimuth[meas_param])] 
-        wind_direction = np.radians(np.linspace(Atmospheric_Scenario.wind_direction[0],Atmospheric_Scenario.wind_direction[1],360))
+        wind_direction = np.radians(np.linspace(Atmospheric_Scenario.wind_direction[0],Atmospheric_Scenario.wind_direction[1],720))
         
         
         #%% 2) Lidars' position and measuring angles. The measuring angles are calculated based on the position of the lidars and the measuring points
