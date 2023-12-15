c37879aa26995c30aa31596502ab5de2f3751271
Francisco Costa <costa@ifb.uni-stuttgart.de>
2023-12-15 17:39:48 +0100

    Updating working directories

--- Main/Qlunc_Instantiate.py
+++ Main/Qlunc_Instantiate.py
@@ -47,7 +47,7 @@ import numpy as np
 # from Utils.Qlunc_ImportModules import *
 
 # import  Qlunc.UQ_Functions.UQ_Photonics_Classes as uphc, Qlunc.UQ_Functions.UQ_Optics_Classes as uopc, Qlunc.UQ_Functions.UQ_Lidar_Classes as ulc,  Qlunc.UQ_Functions.UQ_ProbeVolume_Classes as upbc,  Qlunc.UQ_Functions.UQ_SignalProcessor_Classes as uspc
-# os.chdir(os.path.normpath(os.path.join(os.path.dirname(__file__),"..\\")))
+os.chdir(os.path.normpath(os.path.join(os.path.dirname(__file__),"..\\")))
 # importing  uncertainty functions
 # import  UQ_Functions.UQ_Photonics_Classes as uphc,UQ_Functions.UQ_Optics_Classes as uopc,UQ_Functions.UQ_Lidar_Classes as ulc, UQ_Functions.UQ_ProbeVolume_Classes as upbc, UQ_Functions.UQ_SignalProcessor_Classes as uspc
 
@@ -56,14 +56,14 @@ import numpy as np
 
 
 #%% Running Qlunc_Classes.py:
-with open ('./Qlunc_inputs.yml') as file: # WHere the yaml file is in order to get the input data
+with open ('.\\Main\\Qlunc_inputs.yml') as file: # WHere the yaml file is in order to get the input data
     Qlunc_yaml_inputs={}
     docs = yaml.load_all(file, Loader=yaml.FullLoader)
     for doc in docs:      
         for k, v in doc.items():           
             Qlunc_yaml_inputs.setdefault(k,v)  # save a dictionary with the data coming from yaml file 
 
-
+# pdb.set_trace()
 import  UQ_Functions.UQ_Photonics_Classes as uphc,UQ_Functions.UQ_Optics_Classes as uopc,UQ_Functions.UQ_Lidar_Classes as ulc, UQ_Functions.UQ_ProbeVolume_Classes as upbc, UQ_Functions.UQ_SignalProcessor_Classes as uspc
 from Utils.Qlunc_ImportModules import *
 
