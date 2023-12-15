24d08066b50dfd3bbbd19c63ded4f6b78c40230b
Francisco Costa <costa@ifb.uni-stuttgart.de>
2023-12-15 16:16:15 +0100

    Update GUI_App.py

--- Main/GUI_App.py
+++ Main/GUI_App.py
@@ -11,42 +11,41 @@ from tkinter.filedialog import asksaveasfilename
 import customtkinter as CTk
 import pdb
 import os
-import subprocess
-import sys,inspect,os
-import numpy as np 
-import scipy.interpolate as itp 
-import pandas as pd 
-import numbers
-import pdb
-from scipy.optimize import curve_fit
-import itertools
-import functools
-from mpl_toolkits.mplot3d import Axes3D
-import matplotlib.pyplot as plt
-from functools import reduce
-from operator import getitem
-import time
-import yaml
-import pylab
-import math
+# import sys,inspect,os
+# import numpy as np 
+# import scipy.interpolate as itp 
+# import pandas as pd 
+# import numbers
+# import pdb
+# from scipy.optimize import curve_fit
+# import itertools
+# import functools
+# from mpl_toolkits.mplot3d import Axes3D
+# import matplotlib.pyplot as plt
+# from functools import reduce
+# from operator import getitem
+# import time
+# import yaml
+# import pylab
+# import math
 from io import StringIO  
-import csv
-from termcolor import colored, cprint 
-import random
-import matplotlib
-import scipy as sc
-from scipy.stats import norm
-from matplotlib.pyplot import cm
-from mpl_toolkits.axes_grid1 import make_axes_locatable
-from scipy.stats import multivariate_normal
-import pickle
-# from celluloid import Camera
-from matplotlib import animation
-import matplotlib.colors as mcolors
-import matplotlib.cm as cmx
-from mpl_toolkits.axes_grid1 import Grid
-import matplotlib.gridspec as gridspec
-from scipy.fft import fft, ifft
+# import csv
+# from termcolor import colored, cprint 
+# import random
+# import matplotlib
+# import scipy as sc
+# from scipy.stats import norm
+# from matplotlib.pyplot import cm
+# from mpl_toolkits.axes_grid1 import make_axes_locatable
+# from scipy.stats import multivariate_normal
+# import pickle
+# # from celluloid import Camera
+# from matplotlib import animation
+# import matplotlib.colors as mcolors
+# import matplotlib.cm as cmx
+# from mpl_toolkits.axes_grid1 import Grid
+# import matplotlib.gridspec as gridspec
+# from scipy.fft import fft, ifft
 
 
 global wd, open_status_name,application_path
