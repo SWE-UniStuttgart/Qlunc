bcbe62e8c1f9f6e29583af40c87388c7af3f2627
Francisco Costa <costa@ifb.uni-stuttgart.de>
2023-12-21 14:38:04 +0100

    Update GUI_App.py

--- Main/GUI_App.py
+++ Main/GUI_App.py
@@ -14,49 +14,26 @@ from tkinter import font
 from tkinter.filedialog import askopenfilename
 from tkinter.filedialog import asksaveasfilename
 import customtkinter as CTk
-import sys,os
-import inspect
+import re,pdb,yaml,pylab,math,csv,pickle,matplotlib,numbers,inspect,random,itertools
 import numpy as np 
 import scipy.interpolate as itp 
 import pandas as pd 
-import numbers
-import pdb
-import re
 from scipy.optimize import curve_fit
-import itertools
-import functools
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 from functools import reduce
 from operator import getitem
-import time
-import yaml
-import pylab
-import math
-import xarray as xr
-import netCDF4 as nc    
-import csv
 from termcolor import colored, cprint 
-import random
-import matplotlib
 import scipy as sc
 from scipy.stats import norm
 from matplotlib.pyplot import cm
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 from scipy.stats import multivariate_normal
-import pickle
-from celluloid import Camera
-from matplotlib import animation
 import matplotlib.colors as mcolors
 import matplotlib.cm as cmx
 from mpl_toolkits.axes_grid1 import Grid
 import matplotlib.gridspec as gridspec
 from scipy.fft import fft, ifft
-from tkinter import *
-from tkinter import font
-from tkinter.filedialog import askopenfilename
-from tkinter.filedialog import asksaveasfilename
-import customtkinter as CTk
 from io import StringIO 
 
 
