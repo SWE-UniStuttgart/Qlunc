668d81a0ab7ce7f8122cb615f1a3382dc7490595
Francisco Costa <costa@ifb.uni-stuttgart.de>
2024-01-29 10:21:02 +0100

    Update UQ_SignalProcessor_Classes.py

--- UQ_Functions/UQ_SignalProcessor_Classes.py
+++ UQ_Functions/UQ_SignalProcessor_Classes.py
@@ -46,10 +46,10 @@ def UQ_ADC(Lidar, Atmospheric_Scenario,cts,Qlunc_yaml_inputs,DataFrame):
     lidar_wavelength     = Qlunc_yaml_inputs['Components']['Laser']['Wavelength'] # wavelength of the laser source.
     fs_av                = Qlunc_yaml_inputs['Components']['ADC']['Sampling frequency']   # sampling frequency
     L                    = 2**Lidar.signal_processor.analog2digital_converter.nbits    #length of the signal.
-    n_fftpoints          = 2**8       # n° of points for each block (fft points).
+    n_fftpoints          = L #2**8      # n° of points for each block (fft points).
     fd                   = 2 * V_ref / lidar_wavelength  # Doppler frequency corresponding to Vref
     n_pulses             = 1        #   % n pulses for averaging the spectra
-    N_MC                 = 10000 # n° MC samples to calculate the uncertainty due to bias in sampling frequency and wavelength
+    N_MC                 = 100000 # n° MC samples to calculate the uncertainty due to bias in sampling frequency and wavelength
     # pdb.set_trace()
     #%% Uncertainty due to hardware noise, signal processing and speckle interference:
     
@@ -63,11 +63,13 @@ def UQ_ADC(Lidar, Atmospheric_Scenario,cts,Qlunc_yaml_inputs,DataFrame):
     
     # Bias in the sampling frequency 
     std_fs_av = Qlunc_yaml_inputs['Components']['ADC']['Uncertainty sampling freq']*fs_av
-    fs        = np.random.normal(fs_av , std_fs_av , N_MC)
+    # fs        = np.random.normal(fs_av , std_fs_av , N_MC)
+    fs        = np.random.uniform(fs_av-std_fs_av , fs_av+std_fs_av , N_MC)
+
     Ts        = 1/fs
 
     
-    # Bias in the laser wavelength
+    # Bias in the laser wavelength 
     stdv_wavelength   = Qlunc_yaml_inputs['Components']['Laser']['stdv Wavelength'] / np.sqrt(3) #; % m
     wavelength_noise  = np.random.normal(lidar_wavelength , stdv_wavelength , N_MC)  #; % Noisy wavelength vector
     e_perc_wavelength = 100 * wavelength_noise / lidar_wavelength
@@ -84,13 +86,13 @@ def UQ_ADC(Lidar, Atmospheric_Scenario,cts,Qlunc_yaml_inputs,DataFrame):
     for ind_pulse in range(N_MC):
         t              = np.array(range(0,n_fftpoints)) * Ts[ind_pulse]
         f              = np.linspace(0 , int(fs[ind_pulse] / 2) , int(math.floor(len(t)) / 2 + 1))
-
+        # pdb.set_trace()
         # Signal + Noise:
         #Create signal and add hardware noise and speckle noise:
-        S1 = hardware_noise + (np.sin(2*np.pi*fd_speckle_noise[ind_pulse]*t))-.1*np.sin(2*np.pi*abs(np.random.normal(0,1.9))*fd_speckle_noise[ind_pulse]*t) + .2*np.sin(2*np.pi*abs(np.random.normal(0,3))*fd_speckle_noise[ind_pulse]*t)+\
-                                .2*np.sin(2*np.pi*abs(np.random.normal(0,6))*fd_speckle_noise[ind_pulse]*t) + .3*np.sin(2*np.pi*abs(np.random.normal(0,1))*fd_speckle_noise[ind_pulse]*t) #; % Adding up Signal contributors
+        S1 = hardware_noise + (np.sin(2*np.pi*fd_speckle_noise[ind_pulse]*t))#-.1*np.sin(2*np.pi*abs(np.random.normal(0,1.9))*fd_speckle_noise[ind_pulse]*t) + .2*np.sin(2*np.pi*abs(np.random.normal(0,3))*fd_speckle_noise[ind_pulse]*t)+\
+                                #.2*np.sin(2*np.pi*abs(np.random.normal(0,6))*fd_speckle_noise[ind_pulse]*t) + .3*np.sin(2*np.pi*abs(np.random.normal(0,1))*fd_speckle_noise[ind_pulse]*t) #; % Adding up Signal contributors
         # pdb.set_trace()
-        S=S1/S1.max()
+        S = S1 / S1.max()
         """Uniform quantization approach
     
         Notebook: C2/C2S2_DigitalSignalQuantization.ipynb
@@ -127,7 +129,7 @@ def UQ_ADC(Lidar, Atmospheric_Scenario,cts,Qlunc_yaml_inputs,DataFrame):
     Stdv_fpeak = np.std(fd_peak)
     Stdv_vlos  = np.std(vlos_MC)
     mean_vlos  = np.mean(vlos_MC)
-    # pdb.set_trace()
+    pdb.set_trace()
     # Store data
     DataFrame['Uncertainty ADC'] = {'Stdv Doppler f_peak [Hz]':np.array(Stdv_fpeak)*np.linspace(1,1,len(Atmospheric_Scenario.temperature)),'Stdv wavelength [m]':stdv_wavelength,'Stdv Vlos [m/s]':Stdv_vlos}
     return DataFrame
